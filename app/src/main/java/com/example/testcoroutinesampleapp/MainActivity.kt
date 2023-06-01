5f13a4edf4796231546f6a904daf6eeb86989397
Zach Olbrys <ZOlbrys@gmail.com>
2023-06-01 18:00:36 -0400

    Initial commit - basic project structure working.  Mostly empty fragment hosted in an otherwise empty activity.

---
+++ app/src/main/java/com/example/testcoroutinesampleapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.testcoroutinesampleapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.testcoroutinesampleapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
